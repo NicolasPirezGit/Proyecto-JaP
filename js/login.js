63428d9f386a3aa8233ba1b448f232d2f11f5692
NicolasPirezGit <nicolaspirez799@gmail.com>
2020-08-14 18:25:16 -0300

    cambio de links
    Se cambiaron los enlaces para que lleve al login al entrar al sitio y no a la página de inicio.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,17 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+const INICIO = "file:///C:/Users/equipo/Desktop/Programaci%C3%B3n/1-%20JaP/FASE%202/Curso%20T%C3%A9cnico/Pr%C3%A1cticos/PROYECTO/Materiales/REPOSITORIO%20PROYECTO/Proyecto-JaP/index.html";
+
+
 document.addEventListener("DOMContentLoaded", function(e){
 
+   document.getElementById("submit").addEventListener("click", function(e){
+       let email = document.getElementById("userE");
+       let password = document.getElementById("userP");
+       let camposCompletos = true;
+
+       if(password.value === '' || email.value === '')
+   })
 });
